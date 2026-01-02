9d632dd64cb4f8b33910fce90891e9c763d7cb75
Anusha <Anushagowda1797@gmail.com>
2026-01-02 17:17:34 +0530

    Connection fixed

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,8 +1,8 @@
 // src/lib/mongodb.js
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
-if (!uri) throw new Error("Please add MONGO_URI to .env.local");
+const uri = process.env.MONGODB_URI;
+if (!uri) throw new Error("Please add MONGODB_URI to .env");
 
 let clientPromise;
 
@@ -10,10 +10,11 @@ if (!global._mongoClientPromise) {
   const client = new MongoClient(uri);
   global._mongoClientPromise = client.connect();
 }
+
 clientPromise = global._mongoClientPromise;
 
 export default async function connectDB() {
   const client = await clientPromise;
-  const db = client.db("candle_shop"); // use your DB name
+  const db = client.db("candle_shop");
   return { client, db };
 }
